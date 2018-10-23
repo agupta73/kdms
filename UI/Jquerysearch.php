0d61d3debf0faebb67483d60a5dca7fcc47b66d0
Jugal Dumka <jugaldumka@gmail.com>
2018-10-24 00:58:22 +0530

    JquerySearch and LoginUI

--- UI/Jquerysearch.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include("../Logic/devoteeSearchJquery.php");
-	if(isset($_POST["search-data"])){
-		$searchVal = trim($_POST["search-data"]);
-		$dao = new DAO();
-		echo $dao->searchData($searchVal);
-	}
-?>
--- UI/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* DATABASE CONFIGURATION */
-define('DB_SERVER', 'localhost');
-define('DB_DATABASE', 'skptricksdemo');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-
-?>
